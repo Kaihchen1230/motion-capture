c098d2bf5f844e9874fe27a4862e403699263f03
Kai Hang Chen <kaihang.c@gmail.com>
2020-09-19 12:55:39 -0400

    connect with backend

--- src/App.js
+++ src/App.js
@@ -1,31 +1,55 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Webcam from 'react-webcam';
 import socketIOClient from 'socket.io-client';
+
+const UseInterval = (handler, interval) => {
+	const [intervalId, setIntervalId] = useState();
+	useEffect(() => {
+		const id = setInterval(handler, interval);
+		setIntervalId(id);
+		return () => clearInterval(id);
+	}, []);
+	return () => clearInterval(intervalId);
+};
 
 function App() {
 	const webCamRef = useRef(null);
 
 	const [capturedImageUrl, setCapturedImageUrl] = useState([]);
-	let myInterval;
+	const [intervalId, setIntervalId] = useState();
+
+	let socket = socketIOClient('https://6c627a9603e5.ngrok.io');
 
 	const videoConstraints = {
 		facingMode: 'user',
 	};
 
 	const onStartCapture = () => {
-		let imgLists = [];
-		myInterval = setInterval(() => {
-			const img = webCamRef.current.getScreenshot();
-
-			setCapturedImageUrl((prevState) => [...prevState, img]);
-			// imgLists.push(img);
-			// console.log('this is list: ', imgLists);
-		}, 900);
+		socket.on('connect', onConnect);
 	};
 
 	const onStopCapture = () => {
-		clearInterval(myInterval);
-		console.log('this is capture: ', capturedImageUrl);
+		socket.off('connect', onConnect);
+		// setSocket(null);
+	};
+
+	const onConnect = () => {
+		console.log('this is socket: ', socket);
+
+		let myInterval = setInterval(() => {
+			let frame = webCamRef.current.getScreenshot();
+
+			if (frame && frame.indexOf('data:image/jpeg') !== -1) {
+				frame = frame.replace(/^data:image\/jpeg;base64,/, '');
+				if (frame && frame !== '') {
+					socket.emit('frame', {
+						b64: frame,
+						status: true,
+					});
+				}
+			}
+			console.log('this is frame: ');
+		}, 100);
 	};
 
 	const imageArea = capturedImageUrl.map((imgUrl) => {
@@ -40,15 +64,15 @@ function App() {
 		<div className='App'>
 			<Webcam
 				audio={false}
-				height={240}
-				width={320}
+				height={1080}
+				width={1920}
 				ref={webCamRef}
 				screenshotFormat='image/jpeg'
 				videoConstraints={videoConstraints}
 			/>
+			<button onClick={onStopCapture}>Stop</button>
 			<ul>{imageArea}</ul>
 			<button onClick={onStartCapture}>Start</button>
-			<button onClick={onStopCapture}>Stop</button>
 		</div>
 	);
 }
