b47ca3c0d5dd55e1abc895c82d62cd421820e7af
Kai Hang Chen <kaihang.c@gmail.com>
2020-09-22 10:58:21 -0400

    add arrows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Client from './Client';
 
+
 function App() {
 	const [loadWebCam, setLoadWebCam] = useState(true);
 	const [stopStreaming, setStopStreaming] = useState(false);
@@ -11,17 +12,23 @@ function App() {
 	};
 
 	const handleStopStreaming = () => {
-		setStopStreaming(true);
+		setStopStreaming((prevState) => !prevState);
 		setLoadWebCam(true);
 	};
 
 	return (
 		<div className='App'>
-			{loadWebCam ? (
+			{/* {loadWebCam ? (
 				<button onClick={handleStart}>Start</button>
 			) : (
 				<button onClick={handleStopStreaming}>Stop</button>
-			)}
+				) } */}
+
+			<button onClick={handleStopStreaming}>
+				{!stopStreaming ? 'stop' : 'start'}
+			</button>
+
+			{/* <button onClick={handleStopStreaming}>Stop</button> */}
 
 			{/* <button onClick={ handleClick }>start</button> */}
 
--- src/Client.js
+++ src/Client.js
@@ -1,70 +1,127 @@
 import React, { useState, useEffect, useRef } from 'react';
 import socketIOClient from 'socket.io-client';
 import Webcam from 'react-webcam';
+import 'antd/dist/antd.css';
+import { Row, Col } from 'antd';
+import {
+	CaretLeftFilled,
+	CaretRightFilled,
+	CaretUpFilled,
+	CaretDownFilled,
+} from '@ant-design/icons';
 const ENDPOINT = 'http://localhost:5000';
 
 const Client = (props) => {
 	const webCamRef = useRef(null);
-	const [response, setResponse] = useState('');
-	const [firstTime, setFirstTime] = useState(false);
+	const [screenshot, setScreenshot] = useState('');
+	const [direction, setDirection] = useState(null);
 	const videoConstraints = {
 		facingMode: 'user',
 	};
 
-	useEffect(() => {
-		console.log('this is useeff');
-		let socket = socketIOClient(ENDPOINT);
+	// useEffect(() => {
+	// 	console.log('this is useeff');
+	// 	let socket = socketIOClient(ENDPOINT);
 
-		let myInterval = null;
+	// 	let myInterval = null;
 
-		socket.on('connect', () => {
-			myInterval = setInterval(() => {
-				let frame = webCamRef.current.getScreenshot();
-				// console.log('tis is frame: ', frame);
-				if (frame && frame.indexOf('data:image/jpeg') !== -1) {
-					frame = frame.replace(/^data:image\/jpeg;base64,/, '');
-					if (frame && frame !== '') {
-						// console.log(
-						// 	'this is res: ',
-						// 	response,
-						// 	' and this is length: ',
-						// 	response.length,
-						// );
-						console.log('this is props.start: ', props.start);
-						socket.emit('frame', {
-							b64: frame,
-							status: props.start,
-							width: 1080,
-							height: 1080,
-						});
-					}
-				}
-			}, 10000 / 40);
-		});
-		setFirstTime(true);
-		socket.on('response', function (res) {
-			console.log(res);
-			setResponse(`data:image/jpeg;base64,${res.b64}`);
-		});
+	// 	socket.on('connect', () => {
+	// 		myInterval = setInterval(() => {
+	// 			let frame = webCamRef.current.getScreenshot();
+	// 			// console.log('tis is frame: ', frame);
+	// 			if (frame && frame.indexOf('data:image/jpeg') !== -1) {
+	// 				frame = frame.replace(/^data:image\/jpeg;base64,/, '');
+	// 				if (frame && frame !== '') {
+	// 					// console.log(
+	// 					// 	'this is res: ',
+	// 					// 	screenshot,
+	// 					// 	' and this is length: ',
+	// 					// 	screenshot.length,
+	// 					// );
+	// 					console.log('this is props.start: ', props.start);
+	// 					socket.emit('frame', {
+	// 						b64: frame,
+	// 						status: screenshot.length > 0,
+	// 						width: 1080,
+	// 						height: 1080,
+	// 					});
+	// 				}
+	// 			}
+	// 		}, 10000 / 40);
+	// 	});
 
-		// console.log('this is socket in else: ', socket);
-		return () => {
-			socket.disconnect();
-			clearInterval(myInterval);
-		};
-	}, [props.start]);
-	// console.log('this is response.length before return: ', response.length);
+	// 	socket.on('response', function (res) {
+	// 		console.log(res);
+	// 		setScreenshot(`data:image/jpeg;base64,${res.b64}`);
+	// 		setDirection(res.res);
+	// 	});
+
+	// 	return () => {
+	// 		socket.disconnect();
+	// 		clearInterval(myInterval);
+	// 	};
+	// }, [screenshot]);
+	// console.log('this is screenshot.length before return: ', screenshot.length);
 	return (
 		<div>
-			<img src={response} alt='' />
-			<Webcam
-				audio={false}
-				height={response.length > 0 ? 0 : 1080}
-				width={response.length > 0 ? 0 : 1080}
-				ref={webCamRef}
-				screenshotFormat='image/jpeg'
-				videoConstraints={videoConstraints}
-			/>
+			<p>{direction}</p>
+			<Row>
+				<Col span={24}>
+					<CaretUpFilled
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-around',
+							fontSize: '8em',
+							height: '100%',
+						}}
+					/>
+				</Col>
+				<Col span={4}>
+					<CaretLeftFilled
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-around',
+							fontSize: '8em',
+							height: '100%',
+						}}
+					/>
+				</Col>
+				<Col span={16}>
+					<img src={screenshot} alt='' />
+					<Webcam
+						audio={false}
+						height={screenshot.length > 0 ? 0 : '100%'}
+						width={screenshot.length > 0 ? 0 : '100%'}
+						ref={webCamRef}
+						screenshotFormat='image/jpeg'
+						videoConstraints={videoConstraints}
+					/>
+				</Col>
+				<Col span={4}>
+					<CaretRightFilled
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-around',
+							fontSize: '8em',
+							height: '100%',
+						}}
+					/>
+				</Col>
+				<Col span={24}>
+					<CaretDownFilled
+						style={{
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-around',
+							fontSize: '8em',
+							height: '100%',
+						}}
+					/>
+				</Col>
+			</Row>
 		</div>
 	);
 };
